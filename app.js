ed420a024e02ae333da98054fc3e653b188f8653
Lockett Pundt <kalifornium@hotmail.com>
2020-07-06 20:19:28 -0400

    connected mongoDB. imported cors.

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: process.env.DB_NAME,
+})
+  .then(() => console.log(`connected to ${process.env.DB_NAME}`))
+  .catch((err) => console.log(err));
+
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
+module.exports = app;
